8f8e5bc3cfcf2a7beb49ce87665d80a334f35392
Vedant2802 <avedant981@gmail.com>
2025-08-03 22:13:54 +0530

    commented out some code

--- vanilla-js/random-examples/index3.js
+++ vanilla-js/random-examples/index3.js
@@ -1,7 +1,7 @@
 const routes = {
   "/": "<h2>Home</h2><p>Welcome to the homepage.</p>",
   "/about": "<h2>About Page</h2><p>This is the about page.</p>",
-  "/myname": "<h2>My Name</h2><p>Hello, Akshat!</p>",
+  // "/myname": "<h2>My Name</h2><p>Hello, Akshat!</p>",
 };
 
 // console.log(location.pathname);
